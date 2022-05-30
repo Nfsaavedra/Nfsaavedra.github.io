cf9ad3e1588bf6f9049c7917db38b1ff9f11f3c0
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2022-05-30 21:22:53 +0100

    vue version

---
+++ portfolio/src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <b-carousel
+      id="carousel-fade"
+      style="background-color: black"
+      fade
+      indicators
+      img-height="480"
+    >
+
+        <b-carousel-slide
+          caption="Hello world!"
+        >
+          <template #img>
+            <img
+              class="d-block img-fluid w-100 overlay"
+              :src="img_code"
+            >
+          </template>
+        </b-carousel-slide>
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+import code from "@/assets/code.jpg"
+
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      img_code: code
+    };
+  }
+}
+</script>
+
+<style scoped>
+.overlay {
+   object-fit: cover;
+   opacity: 0.4;
+}
+</style>
