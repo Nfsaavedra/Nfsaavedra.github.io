ec38e1e2218075fa9353c830b8c19073f260d18d
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2022-05-31 16:11:26 +0100

    Update frontpage

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <b-navbar variant=primary>
-      <div style="display: block; width: 100%;">
-        <div style="font-size: 40pt; color: black">Nuno Saavedra</div>
-        <b-navbar-nav style="margin-left: 27.5%; width: 45%;" align="center" justified=true>
-          <b-nav-item  href="/">Home</b-nav-item >
-          <b-nav-item  href="/about">About Me</b-nav-item>
-          <b-nav-item  href="/projects">Projects</b-nav-item >
-          <b-nav-item  href="/publications">Publications</b-nav-item>
-          <b-nav-item  href="/contacts">Contacts</b-nav-item>
-        </b-navbar-nav>
-      </div>
-    </b-navbar>
+      <b-navbar toggleable="xl" variant=white>
+        <div style="display: block; width: 100%;">
+          <div id="logo-name">Nuno Saavedra</div>
+
+          <b-navbar-toggle target="navbar-toggle-collapse">
+            <template #default="{ expanded }">
+              <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+              <b-icon v-else icon="chevron-bar-down"></b-icon>
+            </template>
+          </b-navbar-toggle>
+
+          <b-collapse id="navbar-toggle-collapse" is-nav>
+            <b-navbar-nav style="margin-left: 25%; width: 50%;" align="center" justified>
+              <b-nav-item  href="/">Home</b-nav-item >
+              <b-nav-item  href="/about">About Me</b-nav-item>
+              <b-nav-item  href="/projects">Projects</b-nav-item >
+              <b-nav-item  href="/publications">Publications</b-nav-item>
+              <b-nav-item  href="/contacts">Contacts</b-nav-item>
+            </b-navbar-nav>
+          </b-collapse>
+        </div>
+      </b-navbar>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+@font-face {
+  font-family: "Lobster";
+  src: local("Lobster"), url("./fonts/Lobster/Lobster-Regular.ttf") format("truetype");
+}
+
+@font-face {
+    font-family: "Nunito";
+    src: url("./fonts/Nunito/NunitoSans-Regular.ttf");
+}
+@font-face {
+    font-family: "Nunito";
+    src: url("./fonts/Nunito/NunitoSans-Bold.ttf");
+    font-weight: bold;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Nunito, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
+  background-color: var(--primary);
 }
 
 nav {
@@ -36,5 +70,19 @@ nav {
       color: #42b983;
     }
   }
+}
+
+#logo-name {
+  font-size: 40pt; 
+  color: black; 
+  font-family: "Lobster", Helvetica, Arial;
+}
+
+.nav-link {
+  font-size: 20px;
+}
+
+.nav-link:hover {
+  color: var(--danger) !important;
 }
 </style>
