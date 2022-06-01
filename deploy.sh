caaa2b8134f3c8e2983679f3371c251a10848419
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2022-06-01 22:46:09 +0100

    Chenge deploy method

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
-git subtree push --prefix $1 origin gh-pages
+#!/bin/bash
+GIT_REPO_URL=git@github.com:Nfsaavedra/Nfsaavedra.github.io.git
+
+mkdir .deploy
+cp -R $1/* .deploy
+cp CNAME .deploy
+cd .deploy
+git init .
+git remote add github $GIT_REPO_URL
+git checkout -b gh-pages
+git add .
+git commit -am "deploy"
+git push github gh-pages --force
+cd ..
+rm -rf .deploy
