05b450541bea95274097cf30284e76606ba5eda2
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2023-04-26 16:17:32 +0100

    Automatic scrapping from ORCID

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -15,9 +15,9 @@
           <b-navbar-nav style="margin-left: 25%; width: 50%;" align="center" justified>
             <b-nav-item  href="/">Home</b-nav-item >
             <b-nav-item  href="/#/about">About Me</b-nav-item>
-            <b-nav-item  href="/#/projects">Projects</b-nav-item >
+            <!-- <b-nav-item  href="/#/projects">Projects</b-nav-item > -->
             <b-nav-item  href="/#/publications">Publications</b-nav-item>
-            <b-nav-item  href="/#/blog">Blog</b-nav-item>
+            <!-- <b-nav-item  href="/#/blog">Blog</b-nav-item> -->
             <b-nav-item  href="/#/contacts">Contacts</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
@@ -26,11 +26,12 @@
     <router-view id="view"/>
     <footer>
       <div class="footer-space" style="margin-left: 12vh"/>
-      <div>©2022-2022 Nuno Saavedra</div>
+      <div>©2022-2023 Nuno Saavedra</div>
       <div class="footer-icons">
-        <b-avatar href="mailto: nuno@saavedra.pt" size="3vh" target="_blank" icon="envelope"></b-avatar>
-        <b-avatar href="https://github.com/Nfsaavedra" size="3vh" target="_blank" icon="github"></b-avatar>
-        <b-avatar href="https://scholar.google.com/citations?user=iYiwTYUAAAAJ" size="3vh" target="_blank" icon="google"></b-avatar>
+        <b-avatar href="mailto: nuno@saavedra.pt" style="background-color: white;" size="3vh" target="_blank" src="img/email.svg"></b-avatar>
+        <b-avatar href="https://github.com/Nfsaavedra" style="background-color: white;" size="3vh" target="_blank" src="img/github.svg"></b-avatar>
+        <b-avatar href="https://scholar.google.com/citations?user=iYiwTYUAAAAJ" style="background-color: #535e79;" size="3vh" target="_blank" src="img/scholar.svg"></b-avatar>
+        <b-avatar href="https://scholar.google.com/citations?user=iYiwTYUAAAAJ" style="background-color: white;" size="3vh" target="_blank" src="img/orcid.svg"></b-avatar>
       </div>
     </footer>
   </div>
--- portfolio/src/views/PublicationsView.vue
+++ portfolio/src/views/PublicationsView.vue
@@ -1,41 +1,82 @@
 <template>
   <div>
     <h1>Publications</h1>
-    <div style="margin-left:8%; margin-right: 8%; overflow-y: scroll; max-height: calc(96% - 40px);">
+    <b-spinner v-if="publications.length == 0" style="width: 15vh; height: 15vh;" label="Loading..."></b-spinner>
+    <div style="margin-left:25%; margin-right: 25%; overflow-y: scroll; max-height: calc(96% - 40px);">
       <b-card 
         :title="publication['title']"
         v-for="(publication, index) in publications" 
         :key="publication['title']"
       >
         <div class="authors">
+          <div style="display: inline; margin-right: 8px;">{{ publication['conference'] }}</div><br>
           <div style="display: inline; margin-right: 8px;">{{ publication['authors'] }} - {{ publication['date'] }}</div>
-          <b-badge pill :href="publication['link']" target="_blank">PDF</b-badge>
+          <b-badge pill :href="publication['link']" target="_blank">DOI</b-badge>
+          <b-badge pill v-if="publication['pdf']" :href="publication['pdf']" target="_blank">PDF</b-badge>
           <b-badge v-for="b in publication['badges']" target="_blank" pill :href="b['link']" :key="b['name']">
             {{ b['name'] }}
           </b-badge>
         </div>
-        <b-card-text style="text-align: justify">
+        <b-card-text v-if="publication['abstract']" style="text-align: justify">
           <b-collapse class="abstract" :id="'more-collapse' + index">
             <span style="font-weight: bold;">Abstract: </span>{{ publication['abstract'] }}
           </b-collapse>
         </b-card-text>
-        <div v-b-toggle="'more-collapse' + index" class="arrow"></div>
+        <div v-if="publication['abstract']" v-b-toggle="'more-collapse' + index" class="arrow"></div>
       </b-card>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import { PUBLICATIONS } from '@/constants.js';
 
 export default {
   name: 'ArticlesView',
   data() {
     return {
-        publications: PUBLICATIONS
+      publications: []
     };
   },
+  async created() {
+    const publications = {};
+    const response = await axios.get('https://pub.orcid.org/v3.0/0000-0003-4148-5991/works');
+    const works = response.data.group;
+
+    for (let i = 0; i < works.length; i++) {
+      const work = (await axios.get('https://pub.orcid.org/v3.0' + works[i]['work-summary'][0]['path'])).data;
+      console.log(work);
+      let contributors = '';
+
+      for (let e = 0; e < work.contributors.contributor.length; e++) {
+        contributors += work.contributors.contributor[e]['credit-name'].value + ", ";
+      }
+      contributors = contributors.slice(0, contributors.length - 2);
+
+      publications[work['url'].value] = {
+        'title': work['title']['title'].value,
+        'authors': contributors,
+        'date': this.getMonthName(work['publication-date']['month'].value) + " " + work['publication-date']['year'].value,
+        'link': work['url'].value,
+        'conference': work['journal-title'].value
+      };
+    }
+
+    for (let i = 0; i < PUBLICATIONS.length; i++) {
+      publications[PUBLICATIONS[i]['doi']]['abstract'] = PUBLICATIONS[i]['abstract'];
+      publications[PUBLICATIONS[i]['doi']]['pdf'] = PUBLICATIONS[i]['pdf'];
+      publications[PUBLICATIONS[i]['doi']]['badges'] = PUBLICATIONS[i]['badges'];
+    }
+
+    this.publications = Object.values(publications);
+  },
   methods: {
+    getMonthName(monthNumber) {
+      const date = new Date();
+      date.setMonth(monthNumber - 1);
+      return date.toLocaleString('en-US', { month: 'long' });
+    },
     abstract_begin(string) {
       let words = string.split(" ");
       let res = "";
@@ -123,6 +164,7 @@ export default {
 
 .authors {
   align-self: flex-start;
+  text-align: left;
   margin-bottom: 6px; 
   color: var(--secondary);
 }
